0ccf2c4bd38eac5d1e450cfe41422e6ae2dabdd1
Ionut Moraru <morion4000@gmail.com>
2018-08-13 09:03:43 +0300

    Update Node-API-Public-Blocks.js

--- src/node/sockets/node-server/API/public/Node-API-Public-Blocks.js
+++ src/node/sockets/node-server/API/public/Node-API-Public-Blocks.js
@@ -7,7 +7,7 @@ class NodeAPIPublicBlocks{
 
     blocks(req, res){
 
-        let block_start = req.params.block_start;
+        let block_start = req.block_start;
 
         try {
 
@@ -27,7 +27,7 @@ class NodeAPIPublicBlocks{
 
     block(req, res){
 
-        let index = req.params.block;
+        let index = req.block;
 
         try {
 
